666a583dc55e60c3f9065f7bb0b10d5cde43b384
VincenzoGemino <165017755+VincenzoGemino@users.noreply.github.com>
2025-06-29 16:33:49 +0000

    newc

--- Lab06/pg.c
+++ Lab06/pg.c
@@ -4,13 +4,7 @@
 
 #include <inv.h>
 
-typedef struct pg_s {
-    char *cod;
-    char *nome;
-    char *classe;
-    stat_t b_stat, eq_stat;
-    tabEquip_t *equip; 
-}
+
 
 int main(int argc, char **argv){
 
---
+++ esame26062025/object.h
@@ -0,0 +1,9 @@
+ #ifndef OBJ_H
+ #define OBJ_H
+
+ #include <stdlib.h>
+ #include <stdio.h>
+ #include <object.c>
+ 
+
+ typdef struct objects* oggetti;
